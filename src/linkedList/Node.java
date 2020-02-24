1f449bef75896b32ffbfd4a244aaedc34a8e7a6b
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-02-24 19:42:40 +0530

    make getter and setter of Node class

--- src/linkedList/Node.java
+++ src/linkedList/Node.java
@@ -8,5 +8,21 @@ public class Node {
         this.data = data;
     }
 
+    public Node getData() {
+        return data;
+    }
+
+    public void setData(Node data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 
 }
