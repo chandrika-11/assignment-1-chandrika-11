ffcd3edaf176751334de00e857d5590689021c16
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-02-24 18:36:40 +0530

    add constructor

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,52 +1,17 @@
 package person;
-
+import contactsList.ContactList;
 public class Person {
     private String firstName;
     private String lastName;
-    private MyContactList myContactList;
+    private ContactList contactList;
     private String emailAddress;
 
-    public Person(String firstName, String lastName, MyContactList myContactList, String emailAddress) {
+    public Person(String firstName, String lastName, ContactList contactList, String emailAddress) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.myContactList = myContactList;
+        this.contactList = contactList;
         this.emailAddress = emailAddress;
     }
 
-    @Override
-    public String toString() {
-        return firstName + " " + lastName;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public MyContactList getMyContactList() {
-        return myContactList;
-    }
-
-    public void setMyContactList(MyContactList myContactList) {
-        this.myContactList = myContactList;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
 }
