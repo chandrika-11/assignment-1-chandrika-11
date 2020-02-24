36ccd9d53dafe952386a7d825ba7085e3b4d4afd
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-02-24 20:36:15 +0530

    add a method insertInAlphabeticOrder()

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import contacts.Contacts;
+
 public class LinkedList<C> {
     private Node<C> head;
 
@@ -13,6 +15,24 @@ public class LinkedList<C> {
 
     public void setHead(Node<C> head) {
         this.head = head;
+    }
+
+    public int insertInAlphabeticOrder(Node<Contacts> newNode) {
+        int position = 1;
+        if (head == null || head.toString().toLowerCase().compareTo(newNode.toString().toLowerCase()) > 0) {
+            newNode.setNext((Node<Contacts>) head);
+            setHead((Node<C>) newNode);
+            return position;
+        } else {
+            Node<C> temp = head;
+            while (temp.getNext() != null && temp.getNext().toString().toLowerCase().compareTo(newNode.toString().toLowerCase()) <= 0) {
+                position++;
+                temp = temp.getNext();
+            }
+            newNode.setNext((Node<Contacts>) temp.getNext());
+            temp.setNext((Node<C>) newNode);
+        }
+        return ++position;
     }
 
 
