e369b210d957a383b4ae6b7bc0d02bf179c3b8d4
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-02-24 20:38:06 +0530

    add a method to delete a node

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -2,18 +2,18 @@ package linkedList;
 
 import contacts.Contacts;
 
-public class LinkedList<C> {
-    private Node<C> head;
+public class LinkedList<T> {
+    private Node<T> head;
 
     public LinkedList() {
         head = null;
     }
 
-    public Node<C> getHead() {
+    public Node<T> getHead() {
         return head;
     }
 
-    public void setHead(Node<C> head) {
+    public void setHead(Node<T> head) {
         this.head = head;
     }
 
@@ -21,18 +21,40 @@ public class LinkedList<C> {
         int position = 1;
         if (head == null || head.toString().toLowerCase().compareTo(newNode.toString().toLowerCase()) > 0) {
             newNode.setNext((Node<Contacts>) head);
-            setHead((Node<C>) newNode);
+            setHead((Node<T>) newNode);
             return position;
         } else {
-            Node<C> temp = head;
+            Node<T> temp = head;
             while (temp.getNext() != null && temp.getNext().toString().toLowerCase().compareTo(newNode.toString().toLowerCase()) <= 0) {
                 position++;
                 temp = temp.getNext();
             }
             newNode.setNext((Node<Contacts>) temp.getNext());
-            temp.setNext((Node<C>) newNode);
+            temp.setNext((Node<T>) newNode);
         }
         return ++position;
+    }
+
+    public Node<T> delete(int position) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return null;
+        } else {
+            int pos = 1;
+            if (position == 1) {
+                Node<T> deletedHead = head;
+                head = head.getNext();
+                return deletedHead;
+            }
+            Node<T> temp = head;
+            while (pos < position - 1) {
+                temp = temp.getNext();
+                pos += 1;
+            }
+            Node<T> deletedNode = temp.getNext();
+            temp.setNext(temp.getNext().getNext());
+            return deletedNode;
+        }
     }
 
 
