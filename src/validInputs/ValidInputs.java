60099875412bdf31c6b22153ac6843170645774f
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-02-24 19:28:17 +0530

    create a method inputValidString()

--- src/validInputs/ValidInputs.java
+++ src/validInputs/ValidInputs.java
@@ -18,5 +18,21 @@ public class ValidInputs {
         }
     }
 
+    public String inputValidString() {
+        String inputFromUser;
+        while (true) {
+            inputFromUser = scanner.nextLine().trim();
+
+            try {
+                // check if user directly pressed enter key
+                isEmpty(inputFromUser);
+            } catch (MyNoInputException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            return inputFromUser;
+        }
+    }
+
 
 }
