63cc1d34499201abb4302b671f22f76d5c68e88e
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-02-24 18:40:56 +0530

    add parametrised constructor ContactNumber()

--- src/contactNumber/ContactNumber.java
+++ src/contactNumber/ContactNumber.java
@@ -3,5 +3,9 @@ package contactNumber;
 public class ContactNumber {
     private String contactNumber;
 
+    public ContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
 
 }
